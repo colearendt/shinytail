cc8cb794f2a153c364eb1c0e6dce7cf207717f39
Cole Arendt <cole.arendt@outlook.com>
2018-08-24 22:29:22 -0400

    fix function

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,10 @@
 
-write_to_file <- function(filename, sleep = 1, iterations = 1000)
-i <- 0
-while (i < iterations) {
-  i <- i + 1
-  print(paste0("Iteration ", i))
-  cat(paste0("Record ", i, "\n"), file = filename, append = TRUE)
-  Sys.sleep(sleep)
+write_to_file <- function(filename, sleep = 1, iterations = 1000) {
+ i <- 0
+ while (i < iterations) {
+   i <- i + 1
+   print(paste0("Iteration ", i))
+   cat(paste0("Record ", i, "\n"), file = filename, append = TRUE)
+   Sys.sleep(sleep)
+ }
 }
