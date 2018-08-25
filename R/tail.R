a4f165b8a806d28a547599ed7edda9ea14bdbd4b
Cole Arendt <cole.arendt@outlook.com>
2018-08-25 14:57:16 -0400

    change whitespace

--- R/tail.R
+++ R/tail.R
@@ -1,7 +1,12 @@
 
 
 simple_tail <- function(filename) {
-  pr <- processx::process$new("tail", args = c("-f",filename), stdout = "|", stderr = "|")
+  pr <- processx::process$new(
+    "tail",
+    args = c("-f",filename),
+    stdout = "|",
+    stderr = "|"
+    )
   po <- pr$get_output_connection()
   processx::conn_read_lines(po)
   invisible(po)
