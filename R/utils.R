f77bff2e05caff78dfb32b4e2bf150ae95acf7c2
Cole Arendt <cole.arendt@outlook.com>
2018-08-24 22:25:58 -0400

    add utility for logging

---
+++ R/utils.R
@@ -0,0 +1,9 @@
+
+write_to_file <- function(filename, sleep = 1, iterations = 1000)
+i <- 0
+while (i < iterations) {
+  i <- i + 1
+  print(paste0("Iteration ", i))
+  cat(paste0("Record ", i, "\n"), file = filename, append = TRUE)
+  Sys.sleep(sleep)
+}
