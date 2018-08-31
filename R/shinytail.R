8e6249e0fa9351ff8ef3edf3f29f68a416f6cdb1
Cole Arendt <cole.arendt@outlook.com>
2018-08-31 09:51:32 -0400

    add imports

--- R/shinytail.R
+++ R/shinytail.R
@@ -1,3 +1,7 @@
 #' @importFrom processx process
 #' @importFrom processx conn_read_lines
+#' @importFrom shiny invalidateLater
+#' @importFrom shiny div
+#' @importfrom shiny tags
+#' @importFrom shiny verbatimTextOutput
 "_PACKAGE"
